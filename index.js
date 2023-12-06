334d0a1570c588a32e0ad0fb42a6a650977076dd
Gustavo <gustavo@flip.net.br>
2023-12-06 13:21:32 -0300

    comments-gus

--- index.js
+++ index.js
@@ -7,22 +7,17 @@ let rl = readline.createInterface({
 }); // Cria uma interface de leitura e escrita
 const clientSocket = new WebSocket("ws://localhost:3001"); // Cria o cliente socket e conecta ao servidor socket
 
-clientSocket.on("welcome", (welcome) => {
-  // Evento boasvindas, recebe a mensagem do servidor
-  console.log(welcome);
-});
-
 clientSocket.on("message", (resposta) => {
-  getMessage = JSON.parse(resposta).message;
-  getStep = JSON.parse(resposta).step;
-  buildResponse = (step, message) => JSON.stringify({ step, message });
+  getMessage = JSON.parse(resposta).message; // Converte a resposta do servidor para um objeto e pega a mensagem
+  getStep = JSON.parse(resposta).step; // Converte a resposta do servidor para um objeto e pega o step
+  buildResponse = (step, message) => JSON.stringify({ step, message }); // Cria uma função para montar a resposta do cliente
   rl.prompt();
   rl.question(getMessage, (respostaCliente) => {
-    clientSocket.send(buildResponse(getStep,respostaCliente));
+    clientSocket.send(buildResponse(getStep,respostaCliente)); // Envia a resposta do cliente para o servidor
   });
 });
 
-clientSocket.on("close", (resposta) => {
-  console.log("\nObrigado! Volte sempre :)\n");
-  process.exit(0);
+clientSocket.on("close", () => { // Quando o servidor socket é fechado, o cliente socket também é fechado
+  console.log("\nObrigado! Volte sempre :)\n"); 
+  process.exit(0); 
 });
